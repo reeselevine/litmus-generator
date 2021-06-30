39ed87bb39999f7c8b8f30e6bf0eeb11856656ec
Reese <reeselevine@ucsc.edu>
2021-06-30 13:20:44 -0700

    Get kernel formatted correctly

--- litmusgenerator.py
+++ litmusgenerator.py
@@ -11,23 +11,23 @@ class LitmusTest:
     openCL_stress_mem_location = "scratchpad[scratch_locations[get_group_id(0)]]"
     # Returns the first access in the stress pattern
     openCL_stress_first_access = {
-        "store": ["{} = i;".format(stress_mem_location)],
-        "load": ["uint tmp1 = {};".format(stress_mem_location), 
-            "if (tmp1 > 100) {", "{} = get_local_id(0);".format(stress_mem_location), 
+        "store": ["{} = i;".format(openCL_stress_mem_location)],
+        "load": ["uint tmp1 = {};".format(openCL_stress_mem_location), 
+            "if (tmp1 > 100) {", "  break;",
             "}"]
     }
     # Given a first access, returns the second access in the stress pattern
     openCL_stress_second_access = {
         "store": {
-            "store": ["{} = i + 1;".format(stress_mem_location)],
-            "load": ["uint tmp1 = {};".format(stress_mem_location),
-                "if (tmp1 > 100) {", "break;",
+            "store": ["{} = i + 1;".format(openCL_stress_mem_location)],
+            "load": ["uint tmp1 = {};".format(openCL_stress_mem_location),
+                "if (tmp1 > 100) {", "  break;",
                 "}"]
         },
         "load": {
-            "store": ["{} = i;".format(stress_mem_location)],
-            "load": ["uint tmp2 = {};".format(stress_mem_location),
-                "if (tmp2 > 100) {", "break;",
+            "store": ["{} = i;".format(openCL_stress_mem_location)],
+            "load": ["uint tmp2 = {};".format(openCL_stress_mem_location),
+                "if (tmp2 > 100) {", "  break;",
                 "}"]
         }
     }
@@ -183,8 +183,8 @@ class LitmusTest:
            body_statements.append("  const uint {} = mem_locations[{}];".format(variable, mem_loc))
         for thread in self.threads:
             variables = set()
-            thread_statements = self.generate_stress()
-            thread_statements = thread_statements + ["  }", "}", "if (stress_params[2]) {", "  spin(barrier);", "}"]
+            thread_statements = ["if (stress_params[4]) {", "  do_stress(scratchpad, scratch_locations, stress_params[5], stress_params[6]);", "}"]
+            thread_statements = thread_statements + ["if (stress_params[0]) {", "  spin(barrier);", "}"]
             for instr in thread.instructions:
                 if isinstance(instr, self.ReadInstruction):
                     variables.add(instr.variable)
@@ -194,34 +194,33 @@ class LitmusTest:
             thread_statements = ["    {}".format(statement) for statement in thread_statements]
             body_statements = body_statements + ["  {}".format(self.thread_filter(thread.workgroup, thread.local_id, first_thread))] + thread_statements
             first_thread = False
-        body_statements = body_statements + [self.generate_mem_stress()]
+        body_statements = body_statements + ["  \n".join(["  } else if (stress_params[1]) {", "    do_stress(scratchpad, scratch_locations, stress_params[2], stress_params[3]);", "  }"])]
         kernel_args = ["__global atomic_uint* test_data", "__global uint* mem_locations", "__global atomic_uint* results", "__global uint* shuffled_ids","__global atomic_uint* barrier", "__global uint* scratchpad", "__global uint* scratch_locations", "__global uint* stress_params"]
         kernel_func_def = "__kernel void litmus_test(\n  " + ",\n  ".join(kernel_args) + ") {"
         kernel = "\n".join([kernel_func_def] + body_statements + ["}\n"])
         spin_func = self.generate_spin()
-        kernel = "\n\n".join([spin_func, kernel])
+        stress_func = self.generate_stress()
+        kernel = "\n\n".join([spin_func, stress_func, kernel])
         filename = "target/" + self.test_name + ".cl"
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         with open(filename, "w") as output_file:
             output_file.write(kernel)
 
     def generate_stress(self):
-        outer_statements = [
-            "if (stress_params[1]) {",
-            "  for (uint i = 0; i < stress_params[2]; i++) {",
-            "    uint pattern = stress_params[3]"
-        ]
+        body = ["static void do_stress(__global uint* scratchpad, __global uint* scratch_locations, uint iterations, uint pattern) {",
+                "for (uint i = 0; i < iterations; i++) {"]
         i = 0
         for first in self.openCL_stress_first_access:
             for second in self.openCL_stress_second_access[first]:
                 if i == 0:
-                    outer_statements += "    if (pattern == 0) {"
+                    body += ["  if (pattern == 0) {"]
                 else:
-                    outer_statements += "    if (pattern == {}) {{".format(i)
-                outer_statements += self.openCL_stress_first_access[first] + self.openCL_stress_second_access[first][second]
-                outer_statements += "   }"
+                    body += ["  }} else if (pattern == {}) {{".format(i)]
+                body += ["    {}".format(statement) for statement in self.openCL_stress_first_access[first]]
+                body += ["    {}".format(statement) for statement in self.openCL_stress_second_access[first][second]]
                 i += 1
-        return outer_statements
+        body += ["  }", "}"]
+        return "\n".join(["\n  ".join(body), "}"])
 
     def generate_spin(self):
         header = "static void spin(__global atomic_uint* barrier) {"
@@ -235,15 +234,6 @@ class LitmusTest:
             "}"
         ])
         return "\n".join([body, "}"])
-
-    def generate_mem_stress(self):
-        block = [
-            "  } else if (stress_params[0]) {",
-            "  for (uint i = 0; i < {}; i++) {{".format(self.parameter_config["stressIterations"])
-        ]
-        block = block + ["    {}".format(statement) for statement in self.stress_pattern.pattern()]
-        block = block + ["  }", "}"]
-        return "\n    ".join(block)
 
     def thread_filter(self, workgroup, thread, first_thread):
         if first_thread:
