a50eac706fb8c69321488ea7bea964959d17991d
Reese Levine <reeselevine1@gmail.com>
2021-07-07 14:58:21 -0700

    Fix bugs in generated shader

--- wgsllitmusgenerator.py
+++ wgsllitmusgenerator.py
@@ -3,26 +3,26 @@ import litmusgenerator
 
 class WgslLitmusTest(litmusgenerator.LitmusTest):
 
-    wgsl_stress_mem_location = "scratchpad.value[scratch_locations.value[workgroup_id[0]]]"
+    wgsl_stress_mem_location = "scratchpad.value[scratch_locations.value[workgroup_id]]"
     # returns the first access in the stress pattern
     wgsl_stress_first_access = {
         "store": ["{} = i;".format(wgsl_stress_mem_location)],
         "load": ["let tmp1: u32 = {};".format(wgsl_stress_mem_location),
-            "if (tmp1 > 100) {", "  break;",
+            "if (tmp1 > 100u) {", "  break;",
             "}"]
     }
     # given a first access, returns the second access in the stress pattern
     wgsl_stress_second_access = {
         "store": {
-            "store": ["{} = i + 1;".format(wgsl_stress_mem_location)],
+            "store": ["{} = i + 1u;".format(wgsl_stress_mem_location)],
             "load": ["let tmp1: u32 = {};".format(wgsl_stress_mem_location),
-                "if (tmp1 > 100) {", "  break;",
+                "if (tmp1 > 100u) {", "  break;",
                 "}"]
         },
         "load": {
             "store": ["{} = i;".format(wgsl_stress_mem_location)],
             "load": ["let tmp2: u32 = {};".format(wgsl_stress_mem_location),
-                "if (tmp2 > 100) {", "  break;",
+                "if (tmp2 > 100u) {", "  break;",
                 "}"]
         }
     }
@@ -35,10 +35,10 @@ class WgslLitmusTest(litmusgenerator.LitmusTest):
 
     def generate_thread_header(self):
         return [
-            "if (stress_params.value[4]) {",
-            "  do_stress(stress_params.value[5], stress_params.value[6]);",
+            "if (stress_params.value[4] == 1u) {",
+            "  do_stress(stress_params.value[5], stress_params.value[6], workgroup_id[0]);",
             "}",
-            "if (stress_params.value[0]) {",
+            "if (stress_params.value[0] == 1u) {",
             "  spin();",
             "}"
         ]
@@ -57,7 +57,7 @@ class WgslLitmusTest(litmusgenerator.LitmusTest):
             "[[group(0), binding(4)]] var<storage, read_write> barrier : AtomicMemory;",
             "[[group(0), binding(5)]] var<storage, read_write> scratchpad : Memory;",
             "[[group(0), binding(6)]] var<storage, read_write> scratch_locations : Memory;",
-            "[[group(0), binding(6)]] var<storage, read_write> stress_params : Memory;"
+            "[[group(0), binding(7)]] var<storage, read_write> stress_params : Memory;"
         ]
         return "\n".join(bindings)
 
@@ -65,14 +65,14 @@ class WgslLitmusTest(litmusgenerator.LitmusTest):
         return "\n\n".join([self.generate_types(), self.generate_bindings()])
 
     def generate_stress(self):
-        body = ["[[stage(compute)]] fn do_stress(iterations: u32, pattern: u32) {", "for(var i: u32 = 0; i < iterations; i = i + 1) {"]
+        body = ["fn do_stress(iterations: u32, pattern: u32, workgroup_id: u32) {", "for(var i: u32 = 0u; i < iterations; i = i + 1u) {"]
         i = 0
         for first in self.wgsl_stress_first_access:
             for second in self.wgsl_stress_second_access[first]:
                 if i == 0:
-                    body += ["  if (pattern == 0) {"]
+                    body += ["  if (pattern == 0u) {"]
                 else:
-                    body += ["  }} elseif (pattern == {}) {{".format(i)]
+                    body += ["  }} elseif (pattern == {}u) {{".format(i)]
                 body += ["    {}".format(statement) for statement in self.wgsl_stress_first_access[first]]
                 body += ["    {}".format(statement) for statement in self.wgsl_stress_second_access[first][second]]
                 i += 1
@@ -81,15 +81,15 @@ class WgslLitmusTest(litmusgenerator.LitmusTest):
 
     def generate_spin(self):
         body = [
-            "[[stage(compute)]] fn spin() {",
-            "  var i : u32 = 0;",
-            "  var bar_val : u32 = atomicAdd(&barrier.value[0], 1);",
+            "fn spin() {",
+            "  var i : u32 = 0u;",
+            "  var bar_val : u32 = atomicAdd(&barrier.value[0], 1u);",
             "  loop {",
-            "    if (i == 1024 || bar_val >= {}) {{".format(len(self.threads)),
+            "    if (i == 1024u || bar_val >= {}u) {{".format(len(self.threads)),
             "      break;",
             "    }",
             "    bar_val = atomicLoad(&barrier.value[0]);",
-            "    i = i + 1;",
+            "    i = i + 1u;",
             "  }",
             "}"
         ]
@@ -99,7 +99,7 @@ class WgslLitmusTest(litmusgenerator.LitmusTest):
         return "let {} = atomicLoad(&test_data.value[{}]);".format(instr.variable, instr.mem_loc)
 
     def write_repr(self, instr):
-        return "atomicStore(&test_data.value[{}], {});".format(instr.mem_loc, instr.value)
+        return "atomicStore(&test_data.value[{}], {}u);".format(instr.mem_loc, instr.value)
 
     def fence_repr(self, instr):
         pass
@@ -116,10 +116,13 @@ class WgslLitmusTest(litmusgenerator.LitmusTest):
 
     def generate_stress_call(self):
         return [
-            "  } elseif (stress_params.value[1]) {",
-            "    do_stress(stress_params.value[2], stress_params.value[3]);",
+            "  } elseif (stress_params.value[1] == 1u) {",
+            "    do_stress(stress_params.value[2], stress_params.value[3], workgroup_id[0]);",
             "  }"
         ]
 
     def generate_shader_def(self):
-        return "[[stage(compute)]] fn main([[builtin(workgroup_id) workgroup_id : vec3<u32>, [[builtin(global_invocation_id)]] global_invocation_id : vec3<u32>, [[builtin(local_invocation_index)]] local_invocation_index : u32) {"
+        return "\n".join([
+            "let workgroupXSize = 1;"
+            "[[stage(compute), workgroup_size(workgroupXSize)]] fn main([[builtin(workgroup_id)]] workgroup_id : vec3<u32>, [[builtin(global_invocation_id)]] global_invocation_id : vec3<u32>, [[builtin(local_invocation_index)]] local_invocation_index : u32) {"
+        ])
