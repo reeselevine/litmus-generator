6af8c511c523b8c3cec535b0e2ab5087829db3aa
Reese Levine <reeselevine1@gmail.com>
2021-12-29 19:07:26 -0800

    Work on new vulkan setup

--- test-runner/test_runner.cpp
+++ test-runner/test_runner.cpp
@@ -3,51 +3,72 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <chrono>
 #include <easyvk.h>
 
 using namespace std;
 
 const int size = 4;
 
-void run(string &shader_file, map<string, string> params) {
+void run(string &shader_file, map<string, string> params)
+{
   auto instance = easyvk::Instance(false);
-	auto device = instance.devices().at(0);
-	auto a = easyvk::Buffer(device, size);
-	auto b = easyvk::Buffer(device, size);
-	auto c = easyvk::Buffer(device, size);
-	for (int i = 0; i < size; i++) {
-		a.store(i, i);
-		b.store(i, i + 1);
-		c.store(i, 0);
-	}
-	std::vector<easyvk::Buffer> bufs = {a, b, c};
-	auto program = easyvk::Program(device, shader_file.c_str(), bufs);
-	program.setWorkgroups(size);
-	program.setWorkgroupSize(1);
-	program.prepare();
-	program.run();
-	for (int i = 0; i < size; i++) {
-		std::cout << "c[" << i << "]: " << c.load(i) << "\n";
-		assert(c.load(i) == a.load(i) + b.load(i));
-	}
-	program.teardown();
-	a.teardown();
-	b.teardown();
-	c.teardown();
-	device.teardown();
-	instance.teardown();
+  auto device = instance.devices().at(0);
+  auto a = easyvk::Buffer(device, size);
+  auto b = easyvk::Buffer(device, size);
+  auto c = easyvk::Buffer(device, size);
+  for (int i = 0; i < size; i++)
+  {
+    a.store(i, i);
+    b.store(i, i + 1);
+    c.store(i, 0);
+  }
+  std::vector<easyvk::Buffer> bufs = {a, b, c};
+  auto program = easyvk::Program(device, shader_file.c_str(), bufs);
+  program.setWorkgroups(size);
+  program.setWorkgroupSize(1);
+  program.prepare();
+  program.run();
+  for (int i = 0; i < size; i++)
+  {
+    std::cout << "c[" << i << "]: " << c.load(i) << "\n";
+    assert(c.load(i) == a.load(i) + b.load(i));
+  }
+  program.teardown();
+  a.teardown();
+  b.teardown();
+  c.teardown();
+  device.teardown();
+  instance.teardown();
 }
 
-map<string, string> read_config(string &config_file) {
-  map<string, string> m;
+int setWorkgroupSize()
+{
+  if (minWorkgroupSize == maxWorkgroupSize)
+  {
+    return minWorkgroupSize;
+  }
+  else
+  {
+    int size = rand() % (maxWorkgroupSize - minWorkgroupSize);
+    return minWorkgroupSize + size;
+  }
+}
+
+map<string, int> read_config(string &config_file)
+{
+  map<string, int> m;
   ifstream in_file(config_file);
   string line;
-  while (getline(in_file, line)) {
+  while (getline(in_file, line))
+  {
     istringstream is_line(line);
     string key;
-    if (getline(is_line, key, '=')) {
-      string value;
-      if (getline(is_line, value)) {
+    if (getline(is_line, key, '='))
+    {
+      int value;
+      if (getline(is_line, value))
+      {
         m[key] = value;
       }
     }
@@ -55,17 +76,27 @@ map<string, string> read_config(string &config_file) {
   return m;
 }
 
-void print_help() {
+void print_help()
+{
   cout << "Usage: ./TestRunner shaderFile paramFile\n";
 }
 
-int main(int argc, char* argv[]) {
-  if (argc < 3) {
+int main(int argc, char *argv[])
+{
+  if (argc < 3)
+  {
     print_help();
-  } else {
+  }
+  else
+  {
+    srand(time(NULL));
     string shaderFile(argv[1]);
     string configFile(argv[2]);
-    map<string, string> params = read_config(configFile);
+    map<string, int> params = read_config(configFile);
+     for (const auto& [key, value] : m) {
+        std::cout << key << " = " << value << "; ";
+    }
+    std::cout << "\n";
     run(shaderFile, params);
   }
   return 0;
