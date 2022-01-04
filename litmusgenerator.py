3c6cf82584fbfd0af7b68a1b2c13d58c05389cc9
Reese Levine <reeselevine1@gmail.com>
2022-01-03 16:06:17 -0800

    Update README

--- litmusgenerator.py
+++ litmusgenerator.py
@@ -11,7 +11,7 @@ def load_config(test_config):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("test_config", help="Path to the test configuration")
-    parser.add_argument("--backend", required=True, help="Backend to use. Valid options are vulkan and wgsl")
+    parser.add_argument("--backend", required=True, help="Backend to use. Valid options are opencl, vulkan, and wgsl")
     parser.add_argument("--gen_result_shader", action="store_true", help="If specified, also generates results aggregation shader for specified test.")
     args = parser.parse_args()
     test_config = load_config(args.test_config)
