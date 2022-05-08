2fe2491733ed82ca2b0159c6e3645265abe2c1f6
Reese Levine <reeselevine1@gmail.com>
2022-05-07 17:02:31 -0700

    Start work on simplifying vulkan setup

--- vulkanlitmustest.py
+++ vulkanlitmustest.py
@@ -2,6 +2,139 @@ import litmustest
 
 class VulkanLitmusTest(litmustest.LitmusTest):
 
+    shader_fn_call = "__kernel void litmus_test ("
+
+    common_test_shader_args = """
+  __global atomic_uint* read_results,
+  __global uint* shuffled_workgroups,
+  __global atomic_uint* barrier,
+  __global uint* scratchpad,
+  __global uint* scratch_locations,
+  __global uint* stress_params
+"""
+
+    atomic_test_shader_args = "  __global atomic_uint* test_locations," + common_test_shader_args
+    non_atomic_test_shader_args = "  __global uint* test_locations," + common_test_shader_args
+
+    shader_fn_call = "__kernel void litmus_test ("
+
+    result_shader_args = """
+  __global atomic_uint* test_locations,
+  __global atomic_uint* read_results,
+  __global atomic_uint* test_results,
+  __global uint* stress_params
+"""
+
+    atomic_workgroup_memory = """TODO"""
+    non_atomic_workgroup_memory = """TODO"""
+
+    memory_location_fns = """
+static uint permute_id(uint id, uint factor, uint mask) {
+  return (id * factor) % mask;
+}
+
+static uint stripe_workgroup(uint workgroup_id, uint local_id, uint testing_workgroups) {
+  return (workgroup_id + 1 + local_id % (testing_workgroups - 1)) % testing_workgroups;
+}
+"""
+
+    test_shader_fns = """
+static void spin(__global atomic_uint* barrier, uint limit) {
+  int i = 0;
+  uint val = atomic_fetch_add_explicit(barrier, 1, memory_order_relaxed);
+  while (i < 1024 && val < limit) {
+    val = atomic_load_explicit(barrier, memory_order_relaxed);
+    i++;
+  }
+}
+
+static void do_stress(__global uint* scratchpad, __global uint* scratch_locations, uint iterations, uint pattern) {
+  for (uint i = 0; i < iterations; i++) {
+    if (pattern == 0) {
+      scratchpad[scratch_locations[get_group_id(0)]] = i;
+      scratchpad[scratch_locations[get_group_id(0)]] = i + 1;
+    } else if (pattern == 1) {
+      scratchpad[scratch_locations[get_group_id(0)]] = i;
+      uint tmp1 = scratchpad[scratch_locations[get_group_id(0)]];
+      if (tmp1 > 100) {
+        break;
+      }
+    } else if (pattern == 2) {
+      uint tmp1 = scratchpad[scratch_locations[get_group_id(0)]];
+      if (tmp1 > 100) {
+        break;
+      }
+      scratchpad[scratch_locations[get_group_id(0)]] = i;
+    } else if (pattern == 3) {
+      uint tmp1 = scratchpad[scratch_locations[get_group_id(0)]];
+      if (tmp1 > 100) {
+        break;
+      }
+      uint tmp2 = scratchpad[scratch_locations[get_group_id(0)]];
+      if (tmp2 > 100) {
+        break;
+      }
+    }
+  }
+}
+"""
+
+    test_shader_common_header = """
+  uint shuffled_workgroup = shuffled_workgroups[get_group_id(0)];
+  if(shuffled_workgroup < stress_params[9]) {
+"""
+
+    test_shader_common_calculations = """
+    uint x_0 = ({}id_0) * stress_params[10] * 2;
+    uint y_0 = ({}permute_id(id_0, stress_params[8], total_ids)) * stress_params[10] * 2 + stress_params[11];
+    uint x_1 = ({}id_1) * stress_params[10] * 2;
+    uint y_1 = ({}permute_id(id_1, stress_params[8], total_ids)) * stress_params[10] * 2 + stress_params[11];
+    if (stress_params[4]) {{
+      do_stress(scratchpad, scratch_locations, stress_params[5], stress_params[6]);
+    }}
+"""
+
+    inter_workgroup_test_shader_code = """
+    uint total_ids = get_local_size(0) * stress_params[9];
+    uint id_0 = shuffled_workgroup * get_local_size(0) + get_local_id(0);
+    uint new_workgroup = stripe_workgroup(shuffled_workgroup, get_local_id(0), stress_params[9]);
+    uint id_1 = new_workgroup * get_local_size(0) + permute_id(get_local_id(0), stress_params[7], get_local_size(0)); """ + test_shader_common_calculations.format("", "", "", "") + """
+    if (stress_params[0]) {
+      spin(barrier, get_local_size(0) * stress_params[9]);
+    }
+"""
+
+    intra_workgroup_test_shader_code = """TODO"""
+
+    test_shader_common_footer = """
+  } else if (stress_params[1]) {
+    do_stress(scratchpad, scratch_locations, stress_params[2], stress_params[3]);
+  }
+}
+"""
+
+    result_shader_common_calculations = """
+  uint total_ids = get_local_size(0) * stress_params[9];
+  uint id_0 = get_global_id(0);
+  uint x_0 = (id_0) * stress_params[10] * 2;
+  uint mem_x_0 = atomic_load(&test_locations[x_0]);
+  uint r0 = atomic_load(&read_results[id_0 * 2]);
+  uint r1 = atomic_load(&read_results[id_0 * 2 + 1]);
+"""
+
+    inter_workgroup_result_shader_code = result_shader_common_calculations + """
+  uint total_ids = get_local_size(0) * stress_params[9];
+  uint y_0 = (permute_id(id_0, stress_params[8], total_ids)) * stress_params[10] * 2 + stress_params[11];
+  uint mem_y_0 = atomic_load(&test_locations[y_0]);
+"""
+
+    intra_workgroup_result_shader_code = """TODO"""
+
+    result_shader_common_footer = """
+}
+    """
+
+
     opencl_stress_mem_location = "scratchpad[scratch_locations[get_group_id(0)]]"
     # returns the first access in the stress pattern
     openCL_stress_first_access = {
@@ -109,7 +242,7 @@ class VulkanLitmusTest(litmustest.LitmusTest):
         return "".join([self.generate_helper_fns()])
 
     def generate_result_meta(self):
-        return "".join([self.generate_helper_fns()]) 
+        return "".join([self.generate_helper_fns()])
 
     def generate_stress(self):
         body = ["static void do_stress(__global uint* scratchpad, __global uint* scratch_locations, uint iterations, uint pattern) {",
@@ -167,7 +300,7 @@ class VulkanLitmusTest(litmustest.LitmusTest):
             return "atomic_work_item_fence(CLK_LOCAL_MEM_FENCE, memory_order_seq_cst, memory_scope_device);"
         else:
             return "atomic_work_item_fence(CLK_GLOBAL_MEM_FENCE, memory_order_seq_cst, memory_scope_device);"
-        
+
 
     def barrier_repr(self, instr):
         if self.workgroup_memory:
