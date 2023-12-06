3557f1f02ddd90f7f45b164f850199d17c346553
Reese Levine <reeselevine@ucsc.edu>
2023-12-06 10:45:04 -0800

    Add some variants

--- test-runner/checker.h
+++ test-runner/checker.h
@@ -8,9 +8,29 @@ int check_mp(vector<uint32_t> results) {
   return results[3];
 }
 
+int check_lb(vector<uint32_t> results) {
+  cout << "r0=1, r1=0 (seq): " << results[0] << "\n";
+  cout << "r0=0, r1=1 (seq): " << results[1] << "\n";
+  cout << "r0=0, r1=0 (interleaved): " << results[2] << "\n";
+  cout << "r0=1, r1=1 (weak): " << results[3] << "\n";
+  return results[3];
+}
+
+int check_sb(vector<uint32_t> results) {
+  cout << "r0=1, r1=0 (seq): " << results[0] << "\n";
+  cout << "r0=0, r1=1 (seq): " << results[1] << "\n";
+  cout << "r0=1, r1=1 (interleaved): " << results[2] << "\n";
+  cout << "r0=0, r1=0 (weak): " << results[3] << "\n";
+  return results[3];
+}
+
 int check_results(vector<uint32_t> results, string test_name) {
   if (test_name == "mp") {
     return check_mp(results);
+  } else if (test_name == "lb") {
+    return check_lb(results);
+  } else if (test_name == "sb") {
+    return check_sb(results);
   }
   return -1;
 }
