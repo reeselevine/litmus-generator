92843389fd8f409299eea9b991d01bda61f64efa
Reese Levine <reeselevine@ucsc.edu>
2023-12-20 10:27:57 -0800

    Set up android

--- test-runner/Android.mk
+++ test-runner/Android.mk
@@ -3,8 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := runner
-LOCAL_C_INCLUDES := ../easyvk/src
-LOCAL_SRC_FILES := runner.cpp ../easyvk/src/easyvk.cpp
+LOCAL_C_INCLUDES := easyvk/src
+LOCAL_SRC_FILES := runner.cpp easyvk/src/easyvk.cpp
 LOCAL_LDLIBS    += -lvulkan -llog
 
 include $(BUILD_EXECUTABLE)
--- test-runner/setup-android.sh
+++ test-runner/setup-android.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# build the spv files
-make
-
 # build the Android binary
 ndk-build NDK_PROJECT_PATH=./ NDK_APPLICATION_MK=./Application.mk APP_BUILD_SCRIPT=./Android.mk
 
 
 # move files to android device
-adb push build /data/local/tmp/time-bounds
-adb push libs/armeabi-v7a/runner /data/local/tmp/time-bounds/
-adb push android-tune.sh /data/local/tmp/time-bounds/
+adb push shaders /data/local/tmp/test-runner/shaders
+adb push libs/armeabi-v7a/runner /data/local/tmp/test-runner/
+adb push tune.sh /data/local/tmp/test-runner/
